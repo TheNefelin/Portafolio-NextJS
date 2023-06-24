b7077196d482525ba81ceb954a2a6b401a43b77b
TheNefelin <metalflap@gmail.com>
2023-06-23 21:17:24 -0400

    First Commit

---
+++ src/components/code.js
@@ -0,0 +1,98 @@
+import styles from './code.module.css'
+import data from '@/data/myExperience'
+
+const fetching = async () => {
+  const response = await fetch("https://bsite.net/metalflap/links");
+  return await response.json();
+}
+
+const prueba = await fetching();
+console.log(prueba)
+
+const myExperience = {
+  tecnologies: ['C#', 'VB', 'JavaScript', 'CSS', 'HTML'],
+  tools: ['.NET', '.NET Core', 'VSCode'],
+  webProyects: {
+    frontEnd: {
+      js: ['Vanilla', 'jQuery', 'React', 'Nextjs 13', 'Node JS', 'Express', 'Handlebars'],
+      css: ['Native', 'Bootstrap']
+    },
+    backEnd: {
+      js: ['Nextjs 13', 'Node JS', 'Express', 'Handlebars'],
+      cSharp: ['Dapper', '.NET Core 5', '.NET Core 6']
+    }
+  },
+  desktopProjects: {
+    fullstack: {
+      vb: ['.NET Framework'],
+      cSharp: ['Dapper', '.NET Core 5', '.NET Core 6']
+    }
+  }
+}
+
+// console.log("-------------------------------")
+// console.log(data[0].tecnologies)
+// console.log(data[0].tools)
+// console.log(data[0].webProyects.frontEnd.js)
+// console.log(data[0].webProyects.frontEnd.css)
+// console.log(data[0].webProyects.backEnd.js)
+// console.log(data[0].webProyects.backEnd.cSharp)
+// console.log(data[0].desktopProjects.fullstack.vb)
+// console.log(data[0].desktopProjects.fullstack.cSharp)
+// console.log("-------------------------------")
+// console.log(myExperience)
+// console.log("-------------------------------")
+// console.log(data[0])
+// console.log("-------------------------------")
+
+const arr = (props) => {
+  return (
+    <>
+      {props.map((e, index) => {
+        return (
+          <span className={styles.color3}>
+            {` ${e}`}
+            {(index < props.length -1 ? <span className={styles.color5}>,</span> : null )}
+          </span>
+        )
+      })}
+    </>
+  )
+}
+
+export default function Code() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.top}>
+        <div className={styles.dots}>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+        </div>
+        <p><strong>_&gt;</strong></p>
+      </div>
+      <div className={styles.bottom}>
+        <div><span className={styles.color1}>{`const`}</span><span className={styles.color5}>{` myExperience `}</span> {` = `}<span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab1}><span className={styles.color2}>{`tecnologies: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.tecnologies)}{` ]`}</span>{`,`}</div>
+        <div className={styles.tab1}><span className={styles.color2}>{`tools: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.tools)}{` ]`}</span>{`,`}</div>
+        <div className={styles.tab1}><span className={styles.color2}>{`webProyects: `}</span><span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab2}><span className={styles.color2}>{`frontEnd: `}</span><span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab3}><span className={styles.color2}>{`js: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.webProyects.frontEnd.js)}{` ]`}</span>{`,`}</div>
+        <div className={styles.tab3}><span className={styles.color2}>{`css: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.webProyects.frontEnd.css)}{` ]`}</span></div>
+        <div className={styles.tab2}><span className={styles.color1}>{`}`}</span></div>
+        <div className={styles.tab2}><span className={styles.color2}>{`backEnd: `}</span><span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab3}><span className={styles.color2}>{`js: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.webProyects.backEnd.js)}{` ]`}</span>{`,`}</div>
+        <div className={styles.tab3}><span className={styles.color2}>{`cSharp: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.webProyects.backEnd.cSharp)}{` ]`}</span></div>
+        <div className={styles.tab2}><span className={styles.color1}>{`}`}</span></div>
+        <div className={styles.tab1}><span className={styles.color1}>{`}`}</span></div>
+        <div className={styles.tab1}><span className={styles.color2}>{`desktopProjects: `}</span><span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab2}><span className={styles.color2}>{`fullstack: `}</span><span className={styles.color1}>{`{`}</span></div>
+        <div className={styles.tab3}><span className={styles.color2}>{`vb: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.desktopProjects.fullstack.vb)}{` ]`}</span>{`,`}</div>
+        <div className={styles.tab3}><span className={styles.color2}>{`cSharp: `}</span><span className={styles.color4}>{` [ `}{arr(myExperience.desktopProjects.fullstack.cSharp)}{` ]`}</span></div>
+        <div className={styles.tab2}><span className={styles.color1}>{`}`}</span></div>
+        <div className={styles.tab1}><span className={styles.color1}>{`}`}</span></div>
+        <div><span className={styles.color1}>{`}`}</span></div>
+      </div>
+    </div>
+  )
+}
