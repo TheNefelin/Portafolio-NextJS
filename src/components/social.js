b63829a8f6d1e392c973cb08a3851028da01e322
TheNefelin <metalflap@gmail.com>
2023-07-04 19:36:58 -0400

    update

--- src/components/social.js
+++ src/components/social.js
@@ -1,8 +1,70 @@
+import styles from './social.module.css'
+import Image from "next/image"
 
-export default function Social() {
+import imgGitHub from '@/assets/social/github.png'
+import imgLinkedin from '@/assets/social/linkedin.png'
+import imgTwitch from '@/assets/social/twitch.png'
+import imgYoutube from '@/assets/social/youtube.png'
+
+export default function Social(props) {
   return (
-    <section>
-      social
+    <section className={styles.section}>
+      <a 
+        className={styles.imgGitHub}
+        href="https://github.com/thenefelin"
+        target='_blank'
+      >
+        <span>
+          <Image
+            src={imgGitHub}
+            width={40}
+            height={40}
+            alt={"GitHub"}
+          />
+        </span>
+      </a>
+      <a 
+        className={styles.imgTwitch}
+        href="https://www.twitch.tv/thenefelin"
+        target='_blank'
+      >
+        <span>
+          <Image
+            src={imgTwitch}
+            width={40}
+            height={40}
+            alt={"Twitch"}
+          />
+        </span>
+      </a>
+      <a 
+        className={styles.imgYoutube}
+        href="https://www.youtube.com/c/TheNefelin"
+        target='_blank'
+      >
+        <span>
+          <Image
+            src={imgYoutube}
+            width={40}
+            height={40}
+            alt={"Youtube"}
+          />
+        </span>
+      </a>
+      <a
+       className={styles.imgLinkedin}
+       href="https://www.linkedin.com/in/nefelin/"
+       target='_blank'
+      >
+        <span>
+          <Image
+            src={imgLinkedin}
+            width={40}
+            height={40}
+            alt={"LinkedIn"}
+          />
+        </span>
+      </a>
     </section>
   )
 }
