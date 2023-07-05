cb2e6c9de147b766470f6263cb19710b321104c1
TheNefelin <metalflap@gmail.com>
2023-07-04 20:26:29 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import styles from './page.module.css'
 import imgBanner from "@/assets/background/fondo_banner.jpg"
 import imgMusic from "@/assets/background/fondo_music.jpg"
 
+import Menu from '@/components/menu'
 import ImgBackground from '@/components/imgBackground'
 import Banner from '@/components/banner'
 import Code from "@/components/code"
@@ -12,7 +13,8 @@ import Social from '@/components/social'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main id='home' className={styles.main}>
+      <Menu />
       <ImgBackground img={imgBanner} alt={"background"} />
       <div className={styles.banner}>
         <Banner />
@@ -21,16 +23,17 @@ export default function Home() {
         <Title txt={"someExperience"} />
         <Code />
       </div>
-      <div className={styles.projects}>
+      <div id="someProjects" className={styles.projects}>
         <Title txt={"someProjects"} />
         <Projects />
       </div>
+      <div id="someHobbies"></div>
       <div className={styles.hobbie}>
         <ImgBackground img={imgMusic} alt={"background"} />
         <Title txt={"someHobbies"} />
         <VideoYoutube />
       </div>
-      <div>
+      <div id="someSocials">
         <Title txt={"someSocials"} />
       </div>
       <Social />
